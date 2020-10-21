5e91b9c7c0a3e4f65c6a91fe4e62e8b646f0d509
simon <simon.tretter@hokify.com>
2020-10-21 15:06:39 +0200

    fix: export multicache

--- ts-cache/src/index.ts
+++ ts-cache/src/index.ts
@@ -8,6 +8,7 @@ export { ExpirationStrategy } from "./strategy/caching/expiration.strategy";
 export { ISyncKeyStrategy } from "./types/key.strategy.types";
 export { Cache } from "./decorator/cache.decorator";
 export { SyncCache } from "./decorator/synccache.decorator";
+export { MultiCache } from "./decorator/multicache.decorator";
 
 export { FsJsonStorage } from "./storage/fs";
 export { MemoryStorage } from "./storage/memory";
